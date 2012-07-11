8374866196815c72fa2d0889e97d13c078bb3a55
yuka2py <yuka2py@gmail.com>
2012-07-11 14:50:26 +0900

    First commit.

---
+++ scrollev.js
@@ -0,0 +1,45 @@
+/**
+ * scrollev.js 0.1.0 - jQuery Plug-In
+ *
+ * Copyright (c) 2012 yuka2py
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ *
+ * Built for jQuery library
+ * http://jquery.com
+ */
+(function($)
+{
+	$.fn.exscroll = function(options) {
+		options = $.extend({
+			onScrollStart: function(){},
+			onScrollMove: function(){},
+			onScrollEnd: function(){},
+			detectionInterval: 100,
+			detectionDuration: 250
+		}, options);
+		
+		var target = $(this);
+		var lastScrollTime = -1;
+		var detectorTimerId;
+		
+		var scrollEventHandler = function() {
+			if (0 > lastScrollTime) {
+				options.onScrollStart.apply(target);
+				detectorTimerId = setInterval(scrollEndEvnetDetector, options.detectionInterval);
+			} else {
+				options.onScrollMove.apply(target);
+			}
+			lastScrollTime = (+ new Date());
+		}
+		
+		var scrollEndEvnetDetector = function() {
+			if (lastScrollTime + options.detectionDuration < (+ new Date())) {
+				clearInterval(detectorTimerId);
+				options.onScrollEnd.apply(target);
+				lastScrollTime = -1;
+			}
+		}
+		target.scroll(scrollEventHandler);
+		return this;
+	};
+})(jQuery);
